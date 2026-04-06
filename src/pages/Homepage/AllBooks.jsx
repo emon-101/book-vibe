84024a11b88d1c45fc6b56a8154893f29ce28d69
emon-101 <emon26.ugv@gmail.com>
2026-04-06 11:30:00 +0600

    added books card and load data dynamically

---
+++ src/pages/Homepage/AllBooks.jsx
@@ -0,0 +1,21 @@
+import React, { use } from 'react';
+import Book from './Book';
+
+const allBooksPromise = fetch('/booksData.json').then(res => res.json());
+const AllBooks = () => {
+    const allBooks = use(allBooksPromise);
+    console.log("Books", allBooks);
+    return (
+        <div className='mt-12 lg:w-4/5 mx-auto px-4'>
+            <h2 className='text-3xl font-bold text-zinc-600 mb-8 text-center'>Books</h2>
+
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    allBooks.map(book => <Book key={book.bookId} book={book} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default AllBooks;
---
+++ src/pages/Homepage/Book.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaRegStar } from 'react-icons/fa';
+
+const Book = ({book}) => {
+  return (
+    <div>
+      <div className="border border-zinc-300 rounded-2xl p-6 flex flex-col gap-4 h-full justify-between cursor-pointer">
+        <figure>
+          <div className="bg-base-300 rounded-2xl py-8">
+            <img
+              className="object-cover h-41.5 mx-auto -rotate-x-15 rotate-y-30 shadow-2xl rounded-sm transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-101"
+              src={book.image}
+              alt={book.bookName}
+            />
+          </div>
+        </figure>
+        {/* Badge */}
+        <div className="flex items-center gap-2">
+          {
+            book.tags.map((badge, idx) => <div key={idx} className="bg-green-50 text-green-500 px-4 py-1 rounded-lg">
+            {badge}
+          </div> )
+          }
+        </div>
+        {/* Title */}
+        <div className="py-4 border-b-2 border-zinc-300 border-dashed">
+          <h2 className="text-2xl font-bold mb-2">{book.bookName}</h2>
+          <p className="text-zinc-500 font-medium">By: {book.publisher}</p>
+        </div>
+        {/* Ratings */}
+        <div className="flex items-center justify-between font-medium">
+          <p>{book.category}</p>
+          <div className="flex items-center gap-1">
+            <span>{book.rating}</span>
+            <span>
+              <FaRegStar />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
